958f99425ce15b8e42e5f0f129ca8285d6c31591
sds <>
2006-07-06 21:17:48 +0000

    added revision info, short description, and GNU GPL license.

--- library/agilent_user.c
+++ library/agilent_user.c
@@ -1,3 +1,34 @@
+/* $Id$ */
+
+/*
+ * $Log$
+ */
+
+/* agilent_user.c
+ * Copyright (C) 2006 Steve D. Sharples
+ *
+ * User library of useful functions for talking to Agilent Infiniium
+ * oscilloscopes using the VXI11 protocol, for Linux. You will also need the
+ * vxi11_X.XX.tar.gz source, currently available from:
+ * http://optics.eee.nottingham.ac.uk/vxi11/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The author's email address is steve.sharples@nottingham.ac.uk
+ */
+
 #include "agilent_user.h"
 
 /* This really is just a wrapper. Only here because folk might be uncomfortable
